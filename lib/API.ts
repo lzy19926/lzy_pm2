e691c5a3ce720153b64784e07b7b9fa8f37a59ab
阳九 <871921282@qq.com>
2024-01-03 01:04:37 +0800

    feat: 重构API代码

--- lib/API.ts
+++ lib/API.ts
@@ -1,56 +1,42 @@
 const path = require("path")
 const god = require('./God')
 const { showTerminalList } = require('./terminal-table')
-
-
-export interface AppConfig {
-  id?: number
-  name?: string
-  cwd?: string
-  script?: string
-  scriptFullPath?: string
-}
-
+import { configManager } from './ConfigManager'
+import type { AppConfig } from "./ConfigManager"
 
 // 判断是否是启动配置文件
 function isConfigFile(cmd: string): boolean {
   return false
 }
 
+// 初步解析cmd  处理 <-->相关配置
+function parseCommand(cmd: string) {
+  const optionList = cmd.split("--").map(o => o.trim()) || []
+  const scriptPath = optionList.shift()
+  const options = {}
 
-
-// 解析处理 <-->相关配置
-function parseOptions(options: string[]) {
-  options.forEach(option => {
+  optionList.forEach(option => {
     switch (option) {
-      case "json": console.log(`handle option: --json`)
+      case "json": Object.assign(options, { json: true })
         break;
-      case "format": console.log(`handle option: --format`)
+      case "format": Object.assign(options, { format: true })
         break;
-      case "startup": console.log(`handle option: --startup`)
+      case "startup": Object.assign(options, { startup: true })
         break;
-      case "flush": console.log(`handle option: --flush`)
+      case "flush": Object.assign(options, { flush: true })
         break;
       default: console.warn(`unknow option: ${option}`)
         break;
     }
   })
+
+  return { scriptPath, options }
 }
 
-// 配置校验
-function verifyConfs(appConfig: AppConfig): AppConfig | Error {
-  return appConfig
-}
+export default class API {
 
-
-class API {
-
-  cwd: string  // 当前项目根目录
-
-  constructor() {
-    this.cwd = process.cwd();
-  }
-
+  private cwd: string = process.cwd(); // 当前终端目录
+  constructor() { }
 
   start(cmd: string) {
     if (isConfigFile(cmd)) {
@@ -60,51 +46,34 @@ class API {
     }
   }
 
-
   _startConfigJson(cmd: string, cb: Function) { }
-
 
   _startScript(cmd: string, cb: Function) {
 
-
-    // 为此任务创建一个config
-    let appConf: AppConfig = {};
-
-    // 处理--系列配置
-    const options = cmd.split("--").map(o => o.trim()) || []
-    appConf.script = options.shift()
-    parseOptions(options)
-
-    // 校验配置
-    if (verifyConfs(appConf) instanceof Error) {
-      return false
-    }
+    // 解析cmd
+    const { scriptPath, options } = parseCommand(cmd)
 
     // 异步执行
-    const that = this
     startNewProcessPath()
 
     // 执行回调
     cb()
 
+    // 通过path启动一个新的进程
+    function startNewProcessPath() {
+
+      const appConfig = configManager.create()
+      appConfig.script = scriptPath
+      appConfig.options = options
+      appConfig.scriptFullPath = path.resolve(appConfig.cwd, appConfig.script)
+
+      god.forkMode(appConfig)
+    }
     // 通过path重启一个进程
     function restartExistingProcessPath() {
 
     }
-    // 通过path启动一个新的进程
-    function startNewProcessPath() {
-      appConf.id = 1 //todo 自增ID
-      appConf.name = "default"
-      appConf.cwd = that.cwd
-      appConf.scriptFullPath = path.join(that.cwd, appConf.script).trim()
 
-
-      god.forkMode(appConf)
-    }
   }
 
-
 }
-
-
-module.exports = API
